9d8e06aed61419f16324c8a74906ad8e965d5a97
Mentesinot <mentesinottegegn@gmail.com>
2021-08-31 07:22:26 -0700

    task

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-* main - Print the word "Holberton"
-*
-* Return: Always 0 (Success)
-*/
-
+ *main- prints Holberton, followed by a new line
+ *Return:always 0 (Success)
+ */
 int main(void)
 {
-	int i;
-	char p[] = "Holberton";
-
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
-	return (0);
+int i = 0;
+char msg[] = "_putchar\n";
+char c;
+while (i <= 8)
+{
+c = msg[i];
+_putchar(c);
+i++;
+}
+return (0);
 }
